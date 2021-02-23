e3b9719435831b337280d799df527bfe6a526734
Patrick Dohm <pd74@evansville.edu>
2021-02-22 19:46:39 -0600

    I have some things to fix, they are in the comments

--- threadsynchro.cpp
+++ threadsynchro.cpp
@@ -2,7 +2,12 @@
 
 #include <iostream>
 #include <stdlib.h>
+#include <time.h>
+#include <pthread.h>
+#include <unistd.h>
 #include "buffer.h"
+
+
 using namespace std;
 
 void *producer(void* param);
@@ -18,20 +23,53 @@ int main(int argc, char *argv[]){
 
 //2) initialize buffer
 
+
+//thread init
+pthread_t tid_producer; //the thread identifier
+pthread_t tid_consumer;
+pthread_attr_t attr; //set of thread attributes
+pthread_attr_init(&attr);
+
 //3) Create producer thread(s)
-
+pthread_create(&tid_producer,&attr,producer,argv[1]); //will need to change this later to incorporate multiple producers and adjust what gets passed to function
 //4) create consumer thread(s)
-
+pthread_create(&tid_consumer,&attr,consumer, argv[1]);
 //5) Sleep
-
+sleep(argv[1]);//need to convert argv[1] to an int first
 //6) exit
+exit(1);
 
 }
 
 void *producer(void* param){
+    buffer_item item;
+    srand(time(NULL));
+    while(true){
+        /* sleep for a random period of time*/
+        sleep(rand()%5);
+        //generate a random number
+        item=rand();
+        if(insert_items(item)){
+            printf("producer made an error inserting an item");
+        }else{
+            printf("producer produced %d\n", item);
+        }
 
+    }
 }
 
 void *consumer(void* param){
-    
+    buffer_item item;
+
+    while(true){
+        //sleep for a random period of time. *note i limit it to 5 seconds for testing purposes
+        sleep(rand()%5);
+
+        if(remove_items(&item)){
+            printf("consumer failed to remove the item");
+        }else{
+            printf("consumer consumed %d\n", item);
+        }
+
+    }
 }
