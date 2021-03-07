d934beea5c22cd78da10c1fffceaa0e44f64f631
Patrick Dohm <pd74@evansville.edu>
2021-03-06 18:54:52 -0600

    fixed linker error

--- buffer.cpp
+++ buffer.cpp
@@ -5,14 +5,14 @@
 buffer_item buffer[BUFFER_SIZE];
 
 
-int insert_item(buffer_item item){
+int insert_items(buffer_item item){
     //insert item into buffer
 
     return 0; //return 0 if successful
     //return -1 indicating an error condition
 }
 
-int remove_item(buffer_item *item){
+int remove_items(buffer_item *item){
 return 0; //return 0 if successful
 //return -1 indicating an error condition
 }
